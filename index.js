dea9bc876501f0142e5c1bb1837dd0ba9e00d49a
mrHacker731 <mrhacker11@duck.com>
2025-04-05 18:58:50 +0530

    add origin to cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://testing-auth-client.onrender.com",
     credentials:true
 }))
 app.use(express.json());
